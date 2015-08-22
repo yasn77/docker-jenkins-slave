077e7d55fc33a6f1156f1b3cef98f1a18e8ce120
Yasser Nabi <yassersaleemi@gmail.com>
2015-08-22 17:46:53 +0100

    Update regexp for docker.env
    SSH Public key was being incorrectly added to authorized_keys file.
    Fixed the regexp to update the docker.env correctly.

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-env | sed -n 's#^\(JENKINS.*\|TZ\)=\(.*\)#\1="\2"#p' > /docker.env
+env | sed -n 's#^\(JENKINS[_a-zA-Z0-9]*\|TZ\)=\(.*\)#\1="\2"#p' > /docker.env
 
 # Generate SSH host keys
 rm -f "/etc/ssh/ssh_host_key"
